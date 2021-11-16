54cb6b4d73890070ad5de2f1b6672b301f0bb50e
Gabriel-Andrade36 <gabrielndcorrea@gmail.com>
2021-11-16 09:53:10 -0300

    feat(meu-primeiro/meu-primeiro2) seguindo o curso, inserido um component manualmente  e um component pela cli
    no curso eela ensinou como um component funciona e como cria-lo manualmente e pelo terminal utilzinando a cli do angular

--- Curso-Angular/src/app/app.module.ts
+++ Curso-Angular/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
