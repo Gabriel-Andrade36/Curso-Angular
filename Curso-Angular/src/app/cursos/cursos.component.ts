f4bed62913cc27bd34dcaed1c9cb370bc5f90d96
Gabriel-Andrade36 <gabrielndcorrea@gmail.com>
2021-11-16 13:41:41 -0300

    update(cursos component) added logic into cursos.component.html

--- Curso-Angular/src/app/cursos/cursos.component.ts
+++ Curso-Angular/src/app/cursos/cursos.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
 export class CursosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  nomePortal: string;
+  constructor() {
+    this.nomePortal = 'http://loiane.training';
   }
 
+  ngOnInit(): void {}
 }
