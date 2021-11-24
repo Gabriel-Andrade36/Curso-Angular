eda92ceebc73bf58c6635196fdeda98b31d69a12
Gabriel-Andrade36 <gabrielndcorrea@gmail.com>
2021-11-24 11:08:20 -0300

    feature(data-binding): event binding examples added

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,11 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.scss'],
+  // styleUrls: ['./data-binding.component.scss'],
+  styles: [
+    `
+      .highlight {
+        background-color: pink;
+        font-weight: bold;
+      }
+    `,
+  ],
 })
 export class DataBindingComponent implements OnInit {
   url: string = 'github.com/Gabriel-Andrade36';
   cursoAngular: boolean = true;
+  pressedKeys: string = '';
+  savedValue: string = '';
+  isMouseOver: boolean = false;
 
   urlImagem1: String = 'http://lorempixel.com.br/500/420/?1';
   urlImagem2: String = 'http://lorempixel.com.br/500/420/?2';
@@ -15,12 +26,29 @@ export class DataBindingComponent implements OnInit {
 
   constructor() {}
 
+  buttonOnClick() {
+    alert('Button works');
+  }
+
+  onKeyPress(evento: KeyboardEvent) {
+    console.log(evento.key);
+    this.pressedKeys = (<HTMLInputElement>evento.target).value;
+  }
+
+  saveValue(valor: string) {
+    this.savedValue = valor;
+  }
+
   getValor() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  eventMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   ngOnInit(): void {}
